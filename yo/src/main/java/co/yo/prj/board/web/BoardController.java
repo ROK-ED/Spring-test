7fe74a12dc2110aeeeddbb9d189ec93f407abf41
admin91 <admin91@218.38.137.27>
2022-01-09 16:34:56 +0900

    어지럽다...

--- yo/src/main/java/co/yo/prj/board/web/BoardController.java
+++ yo/src/main/java/co/yo/prj/board/web/BoardController.java
@@ -1,5 +1,7 @@
 package co.yo.prj.board.web;
 
+import java.util.Date;
+
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -31,40 +33,69 @@ public class BoardController {
 		 */
 	}
 
-	
 	// 글작성 폼으로 이동
 	@RequestMapping("/boardInsertForm.do")
 	public String boardInsertForm() {
 		return "board/boardInsertForm";
 	}
 
-	// 글 작성
-	@RequestMapping("/boardInsert.do")
+	// 글작성 및 수정
+	@RequestMapping("/board.do")
 	public void boardInsert(HttpServletRequest request, ServletResponse response, HttpSession session)
 			throws Exception {
 		BoardVO board = new BoardVO();
+		
+		//cmd insert 안받아지는 문제 및 수정페이지 
+		
+		String cmd = request.getParameter("cmd");
 
-		String title = request.getParameter("title");
-		String content = request.getParameter("content");
-		// String email = (String) session.getAttribute("member_email");
-		board.setBoard_title(title);
-		board.setBoard_content(content);
-		board.setMember_email("admin@admin.com");
+		int board_id;
+		String title;
+		String content;
+		String email;
+		
+		System.out.println("cmd: " + cmd);
+		
+		if (("insert").equals(cmd)) {
+			//BoardVO loginUser = (BoardVO) session.getAttribute("loginUser");
+			title = request.getParameter("title");
+			content = request.getParameter("content");
 
-		boardDao.boardInsert(board);
-		request.getRequestDispatcher("boardSelectList.do").forward(request, response);
+			board.setBoard_title(title);
+			board.setBoard_content(content);
+			board.setMember_email("admin@admin.com");
+
+			boardDao.boardInsert(board);
+			request.getRequestDispatcher("boardSelectList.do").forward(request, response);
+		} else if (("update").equals(cmd)) {
+			// update - board_id, title, content
+			board_id = Integer.parseInt(request.getParameter("board_id"));
+			title = request.getParameter("title");
+			content = request.getParameter("content");
+
+			board.setBoard_id(board_id);
+			board.setBoard_title(title);
+			board.setBoard_content(content);
+			boardDao.boardUpdate(board);
+			request.getRequestDispatcher("boardSelectList.do").forward(request, response);
+		} else if (("delete").equals(cmd)) {
+			board_id = Integer.parseInt(request.getParameter("board_id"));
+
+			boardDao.boardDelete(board);
+			request.getRequestDispatcher("boardSelectList.do").forward(request, response);
+		}
 	}
-	
+
 	// 게시글 디테일
 	@RequestMapping("boardDetail.do")
 	public String boardDetail(HttpServletRequest request, ServletResponse response, HttpSession session, Model model) {
 		int board_id = Integer.parseInt(request.getParameter("board_id"));
-		
+
 		BoardVO board = new BoardVO();
 		board.setBoard_id(board_id);
-		
+
 		model.addAttribute("board", boardDao.boardSelect(board));
-		
+
 		return "board/boardDetail";
 	}
 }
--- yo/src/main/webapp/WEB-INF/views/board/boardInsertForm.jsp
+++ yo/src/main/webapp/WEB-INF/views/board/boardInsertForm.jsp
@@ -10,15 +10,14 @@
 	<div class="container">
 		<div class="row">
 			<div class="col">
-				<form id="frm" action="boardInsert.do" method="get">
+				<form id="frm" action="board.do?cmd=insert" method="get">
 					<div class="form-group">
-						<label for="board_title">Title</label> <input type="text"
-							class="form-control" id="title" name="title" placeholder="Title">
+						<label for="board_title">Title</label> 
+						<input type="text" class="form-control" id="title" name="title" placeholder="Title">
 					</div>
 					<div class="form-group">
 						<label for="board_content">Content</label>
-						<textarea class="form-control" id="content" name="content" rows="15"
-							placeholder="Content"></textarea>
+						<textarea class="form-control" id="content" name="content" rows="15" placeholder="Content"></textarea>
 					</div>
 					<div class="form-row float-right">
 						<input type="submit" class="btn btn-primary" value="submit">
