da13437663c2fb32f00fd3962acdaba0d19d4907
'ed' <'klaizen91@gmail.com'>
2022-01-13 14:14:22 +0900

    Merge remote-tracking branch 'origin/lsh0113'

--- yo/src/main/java/co/yo/prj/place/web/PlaceController.java
+++ yo/src/main/java/co/yo/prj/place/web/PlaceController.java
@@ -2,7 +2,9 @@ package co.yo.prj.place.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PlaceController {
@@ -10,6 +12,13 @@ public class PlaceController {
 	@RequestMapping("placeSelectList.do")
 	public String placeSelectList(Model model) {
 		return "place/placeSelectList";
+	}
+	
+	@PostMapping("/ajaxPlace.do") // 아이디 중복 체크
+	@ResponseBody
+	public String ajaxPlace(String path, Model model ) {
+		String b = null;
+		return b;
 	}
 
 }
