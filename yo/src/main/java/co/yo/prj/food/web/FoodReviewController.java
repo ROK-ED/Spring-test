459b50359849ce9e71e19ad7b04b8da75a9902d9
'yisseol' <'yisseol@naver.com'>
2022-01-13 14:45:46 +0900

    중간커밋

--- yo/src/main/java/co/yo/prj/food/web/FoodReviewController.java
+++ yo/src/main/java/co/yo/prj/food/web/FoodReviewController.java
@@ -15,39 +15,32 @@ import co.yo.prj.food.service.FoodReviewVO;
 public class FoodReviewController {
 
 	@Autowired
-	private FoodReviewService foodReviewService;
+	private FoodReviewService foodReviewDao;
 
 	@RequestMapping("/foodReviewInsert.do")
 	@ResponseBody
 	public String foodReviewInsert(HttpServletRequest request, HttpSession session, FoodReviewVO foodReview) {
-		int review_id = 0;// 나중에 바꿔야함!!!!!!!!!!!!===========
+		int review_id = 0; // 나중에 바꿔야함!!!!!!!!!!!!===========
 		String review_member_email = request.getParameter("review_member_email");
-		System.out.println("이메일 확인" + review_member_email);
 
-		foodReview.setReview_member_email(review_member_email);
-		foodReview.setReview_id(review_id);// ===============이거는 시퀀스로 하기...
-		foodReview.setReview_hit(1);
-		System.out.println(request.getParameter("rate"));
-		
-		
-		String before = request.getParameter("rate");
-		
-		System.out.println("asdfjklhasdkjfhlkjasdhfljhasdjh ======="+before);
-		int rate = Integer.parseInt(request.getParameter("rate"));
-		foodReview.setReview_rate(rate);
-		
-		
-		System.out.println("점수 확인 ======================" + rate);
+		if (review_member_email != null) {
 
-		foodReview.setReview_option("F");
-		foodReview.setReview_food_id(Integer.parseInt(request.getParameter("review_food_id")));
+			System.out.println("이메일 확인" + review_member_email);
 
-		foodReview.setReview_rate(Integer.parseInt(request.getParameter("rate")));
-		foodReview.setReview_subject(request.getParameter("review_textarea"));
-		System.out.println("확인해보기 내용 들어가있나 확인해봐... =========" + foodReview.getReview_subject());
-		foodReviewService.foodReviewInsert(foodReview);
+			foodReview.setReview_id(review_id);// ===============이거는 시퀀스로 하기...
+			foodReview.setReview_member_email(review_member_email);
+			foodReview.setReview_hit(1);
+			
+			
+			foodReview.setReview_place_id(0);
+			foodReview.setReview_option("F");
+			foodReview.setReview_food_id(Integer.parseInt(request.getParameter("review_food_id")));
 
-		if (review_member_email == null) {
+			foodReview.setReview_rate(Integer.parseInt(request.getParameter("rate")));
+			foodReview.setReview_subject(request.getParameter("review_textarea"));
+			foodReviewDao.foodReviewInsert(foodReview);
+			
+		} else {
 			System.out.println("이메일 안됨...ㅠ");
 		}
 
